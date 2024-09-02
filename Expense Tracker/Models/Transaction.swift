e8584c0596bcfde4e268ea0c0fb0ca6a6afc739f
fbara <fbara1@yahoo.com>
2024-09-02 18:01:43 -0400

    Added TransactionCardView and sample transactions.

--- Expense Tracker/Models/Transaction.swift
+++ Expense Tracker/Models/Transaction.swift
@@ -12,12 +12,12 @@ struct Transaction: Identifiable {
     /// Properties
     var title: String
     var remarks: String
-    var amount: String
-    var dateAdded: String
+    var amount: Double
+    var dateAdded: Date
     var category: String
     var tintColor: String
     
-    init(title: String, remarks: String, amount: String, dateAdded: String, category: Category, tintColor: TintColor) {
+    init(title: String, remarks: String, amount: Double, dateAdded: Date, category: Category, tintColor: TintColor) {
         self.title = title
         self.remarks = remarks
         self.amount = amount
@@ -31,3 +31,11 @@ struct Transaction: Identifiable {
         return tints.first(where: { $0.color == tintColor})?.value ?? appTint
     }
 }
+
+/// Sample transaction for UI building
+var sampleTransaction: [Transaction] = [
+    .init(title: "Magic Keyboard", remarks: "Apple Product", amount: 129, dateAdded: .now, category: .expense, tintColor: tints.randomElement()!),
+    .init(title: "Apple Music", remarks: "Subscription", amount: 10.99, dateAdded: .now, category: .expense, tintColor: tints.randomElement()!),
+    .init(title: "iCloud", remarks: "Subscription", amount:  0.99, dateAdded: .now, category: .expense, tintColor: tints.randomElement()!),
+    .init(title: "Payment", remarks: "Payment Received", amount: 2499, dateAdded: .now, category: .income, tintColor: tints.randomElement()!)
+    ]
