ccf137575d0f3e4944788d40e3d34d3edcfebfa6
fbara <fbara1@yahoo.com>
2024-09-02 12:49:01 -0400

    Added extensions for Date & View. Added additionsl views for Category and CardView.

--- Expense Tracker/Helpers/View+Extensions.swift
+++ Expense Tracker/Helpers/View+Extensions.swift
@@ -19,6 +19,29 @@ extension View {
         self
             .frame(maxHeight: .infinity, alignment: alignment)
     }
+    
+    var safeArea: UIEdgeInsets {
+        if let windowScene = (UIApplication.shared.connectedScenes.first as? UIWindowScene) {
+            return windowScene.keyWindow?.safeAreaInsets ?? .zero
+        }
+        
+        return .zero
+    }
+    
+    func format(date: Date, format: String) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        
+        return formatter.string(from: date)
+    }
+    
+    func currencyString(_ value: Double, allowedDigits: Int = 2) -> String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.maximumFractionDigits = allowedDigits
+        
+        return formatter.string(from: .init(value: value)) ?? ""
+    }
 }
 
 
--- Expense Tracker/Views/TabViews/Recents.swift
+++ Expense Tracker/Views/TabViews/Recents.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct Recents: View {
+    /// User properties
+    @AppStorage("userName") private var userName: String = ""
+    /// View Properties
+    @State private var startDate: Date = .now.startOfMonth
+    @State private var endDate: Date = .now.endOfMonth
     var body: some View {
         GeometryReader {
             /// For animation purposes
@@ -17,7 +22,16 @@ struct Recents: View {
                 ScrollView(.vertical) {
                     LazyVStack(spacing: 10, pinnedViews: [.sectionHeaders]) {
                         Section {
+                            /// Date filter button
+                            Button(action: {}, label: {
+                                Text("\(format(date: startDate,format:"MMM-dd-yyyy")) to \(format(date: endDate,format:"MMM-dd-yyyy"))")
+                                    .font(.caption2)
+                                    .foregroundStyle(.gray)
+                            })
+                            .hSpacing(.leading)
                             
+                            /// Card View
+                            CardView(income: 2039, expense: 4098)
                         } header: {
                             HeaderView(size)
                         }
@@ -25,22 +39,79 @@ struct Recents: View {
                     }
                     .padding(15)
                 }
+                .background(.gray.opacity(0.15))
             }
+            
         }
     }
     
     /// Header view
     @ViewBuilder
     func HeaderView(_ size: CGSize) -> some View {
-        VStack(alignment: .leading, spacing: 5, content: {
-            Text("Welcome")
-                .font(.title.bold())
-        })
-        .hSpacing(.leading)
+        HStack(spacing: 10) {
+            VStack(alignment: .leading, spacing: 5, content: {
+                Text("Welcome")
+                    .font(.title.bold())
+                
+                if userName.isEmpty {
+                    Text(userName)
+                        .font(.callout)
+                        .foregroundStyle(.gray)
+                }
+            })
+            .visualEffect { content, geometryProxy in
+                /// Animation that will scale the header view when the user drags the scrollview.
+                content
+                    .scaleEffect(headerScale(size, proxy: geometryProxy), anchor: .topLeading)
+
+            }
+            
+            Spacer(minLength: 0)
+            
+            NavigationLink {
+                
+            } label: {
+                Image(systemName: "plus")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+                    .frame(width: 45, height: 45)
+                    .background(appTint.gradient, in: .circle)
+                    .contentShape(.circle)
+            }
+        }
+        .padding(.bottom, userName.isEmpty ? 10 : 5)
         .background {
-            Rectangle()
+            VStack(spacing: 0) {
+                Rectangle()
+                    .fill(.ultraThinMaterial)
+                
+                Divider()
+            }
+            .visualEffect { content, geometryProxy in
+                content
+                    .opacity(headerBGOpacity(geometryProxy))
+            }
+            .padding(.horizontal, -15)
+            .padding(.top, -(safeArea.top + 15))
         }
     }
+    
+    func headerBGOpacity(_ proxy: GeometryProxy) -> CGFloat {
+        let minY = proxy.frame(in: .scrollView).minY + safeArea.top
+        return minY > 0 ? 0 : (-minY / 15)
+    }
+    
+    func headerScale(_ size: CGSize, proxy: GeometryProxy) -> CGFloat {
+        let minY = proxy.frame(in: .scrollView).minY
+        let screenHeight = size.height
+        
+        let progress = minY / screenHeight
+        let scale = (min(max(progress, 0), 1)) * 0.4
+        
+        return 1 + scale
+    }
+    
 }
 
 #Preview {
